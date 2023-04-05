c51f5f705d0bd8367f5ca24edfd13a067870cf2c
SMN <SMN@SMN-PC>
2023-04-05 21:44:53 +0300

    Slider

---
+++ LifeSpot/Static/JS/slider.js
@@ -0,0 +1,40 @@
+﻿/* Устанавливаем стартовый индекс слайда по умолчанию: */
+let slideIndex = 1;
+
+/* Вызываем функцию, которая реализована ниже: */
+showSlides(slideIndex);
+
+/* Увеличиваем индекс на 1 — показываем следующий слайд: */
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
+function previousSlide() {
+    showSlides(slideIndex -= 1);
+}
+
+/* Устанавливаем текущий слайд: */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Функция перелистывания: */
+function showSlides(n) {
+    /* обращаемся к картинкам: */
+    let slides = document.getElementsByClassName("item");
+
+    /* количество слайдов: */
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+}
